ef02ba8b244b2463263fac94f1a660f5bcd0ebf4
Minju9187 <alswn1583@naver.com>
2024-03-23 16:54:37 +0900

    캘린더 참여 데이터 가져오기

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,8 +1,22 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Topbar from "@/components/Topbar/Topbar";
 import MiniCalendar from "@/components/MiniCalendar";
+import { useEffect } from "react";
+import { db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 export default function Calendar() {
+  const userId = localStorage.getItem("userId");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const collectionRef = doc(db, "calendar", userId);
+      const docSnapshot = await getDoc(collectionRef);
+
+      console.log(docSnapshot.data());
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <Topbar />
