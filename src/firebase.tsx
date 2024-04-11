0d405c07e3292408e278ec5f74ee5bc1bf7caeab
Minju9187 <alswn1583@naver.com>
2024-04-11 16:06:12 +0900

    firebase 파일 수정

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC87iurAwxYDtW8xXiFXQk3sgiLTjkwfGI",
-  authDomain: "check-project-b7db5.firebaseapp.com",
-  projectId: "check-project-b7db5",
-  storageBucket: "check-project-b7db5.appspot.com",
-  messagingSenderId: "444247498539",
-  appId: "1:444247498539:web:78a82f4beb4e46d575ef6e",
-  measurementId: "G-DVKCZVE9E4",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
