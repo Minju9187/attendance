25dc9459cc20313b86d30f519e846bcc6d6b7bcf
Minju9187 <alswn1583@naver.com>
2024-04-12 17:20:17 +0900

    캘린더 점 색깔별 정보 추가

--- src/components/Calendar/MiniCalendar.tsx
+++ src/components/Calendar/MiniCalendar.tsx
@@ -4,6 +4,12 @@ import {
   StyledToday,
   StyledMorningDot,
   StyledAfternoonDot,
+  GreenDot,
+  YellowDot,
+  RedDot,
+  Status,
+  Info,
+  Wrap,
 } from "./MiniCalendarStyle";
 import moment from "moment";
 import { useState } from "react";
@@ -50,53 +56,75 @@ const MiniCalendar = ({ data }: Props) => {
   };
 
   return (
-    <StyledCalendarWrapper>
-      <StyledCalendar
-        value={date}
-        onChange={handleDateChange}
-        formatDay={(_, date) => moment(date).format("D")}
-        formatYear={(_, date) => moment(date).format("YYYY")}
-        formatMonthYear={(_, date) => moment(date).format("YYYY. MM")}
-        calendarType="gregory"
-        showNeighboringMonth={false}
-        next2Label={null}
-        prev2Label={null}
-        minDetail="year"
-        // 오늘 날짜로 돌아오는 기능을 위해 필요한 옵션 설정
-        activeStartDate={activeStartDate === null ? undefined : activeStartDate}
-        onActiveStartDateChange={({ activeStartDate }) =>
-          setActiveStartDate(activeStartDate)
-        }
-        // 오늘 날짜에 '오늘' 텍스트 삽입하고 출석한 날짜에 점 표시를 위한 설정
-        tileContent={({ date, view }) => {
-          const html = [];
-          const formattedDate = moment(date).format("YYYY-MM-DD");
-          const attendance = attendData[formattedDate];
-          if (
-            view === "month" &&
-            date.getMonth() === today.getMonth() &&
-            date.getDate() === today.getDate()
-          ) {
-            html.push(<StyledToday key={"today"}>오늘</StyledToday>);
+    <>
+      <StyledCalendarWrapper>
+        <StyledCalendar
+          value={date}
+          onChange={handleDateChange}
+          formatDay={(_, date) => moment(date).format("D")}
+          formatYear={(_, date) => moment(date).format("YYYY")}
+          formatMonthYear={(_, date) => moment(date).format("YYYY. MM")}
+          calendarType="gregory"
+          showNeighboringMonth={false}
+          next2Label={null}
+          prev2Label={null}
+          minDetail="year"
+          // 오늘 날짜로 돌아오는 기능을 위해 필요한 옵션 설정
+          activeStartDate={
+            activeStartDate === null ? undefined : activeStartDate
           }
-          if (attendance) {
-            html.push(
-              <StyledMorningDot
-                key={moment(date).format("YYYY-MM-DD" + "오전")}
-                style={getDotStyle(attendance.오전 as "출석" | "지각" | "결석")}
-              />
-            );
-            html.push(
-              <StyledAfternoonDot
-                key={moment(date).format("YYYY-MM-DD" + "오후")}
-                style={getDotStyle(attendance.오후 as "출석" | "지각" | "결석")}
-              />
-            );
+          onActiveStartDateChange={({ activeStartDate }) =>
+            setActiveStartDate(activeStartDate)
           }
-          return <>{html}</>;
-        }}
-      />
-    </StyledCalendarWrapper>
+          // 오늘 날짜에 '오늘' 텍스트 삽입하고 출석한 날짜에 점 표시를 위한 설정
+          tileContent={({ date, view }) => {
+            const html = [];
+            const formattedDate = moment(date).format("YYYY-MM-DD");
+            const attendance = attendData[formattedDate];
+            if (
+              view === "month" &&
+              date.getMonth() === today.getMonth() &&
+              date.getDate() === today.getDate()
+            ) {
+              html.push(<StyledToday key={"today"}>오늘</StyledToday>);
+            }
+            if (attendance) {
+              html.push(
+                <StyledMorningDot
+                  key={moment(date).format("YYYY-MM-DD" + "오전")}
+                  style={getDotStyle(
+                    attendance.오전 as "출석" | "지각" | "결석"
+                  )}
+                />
+              );
+              html.push(
+                <StyledAfternoonDot
+                  key={moment(date).format("YYYY-MM-DD" + "오후")}
+                  style={getDotStyle(
+                    attendance.오후 as "출석" | "지각" | "결석"
+                  )}
+                />
+              );
+            }
+            return <>{html}</>;
+          }}
+        />
+      </StyledCalendarWrapper>
+      <Info>
+        <Wrap>
+          <GreenDot />
+          <Status>출석</Status>
+        </Wrap>
+        <Wrap>
+          <YellowDot />
+          <Status>지각</Status>
+        </Wrap>
+        <Wrap>
+          <RedDot />
+          <Status>결석</Status>
+        </Wrap>
+      </Info>
+    </>
   );
 };
 
