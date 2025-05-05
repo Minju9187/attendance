654e4ef407097026b6509907579924bb1cba6f70
Minju9187 <alswn1583@naver.com>
2025-05-05 18:29:15 +0900

    chore: scrollbar패키지 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+import scrollbar from "tailwind-scrollbar";
+import scrollbarHide from "tailwind-scrollbar-hide";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,tsx,jsx}"],
-  plugins: [],
+  plugins: [scrollbar, scrollbarHide],
 };
